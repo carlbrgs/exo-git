105a285aa674133c4e5d509e59d854deb9cf0c4f
Raijin95120 <carlbrgs77@gmail.com>
2023-01-27 00:49:57 +0100

    ajustement taille fenetre + texte

--- helloWorld.py
+++ helloWorld.py
@@ -3,8 +3,8 @@ from tkinter import *
 def HelloWorld(): 
     fenetre = Tk()
     fenetre.title("Hello World")
-    fenetre.geometry("200x200")
-    label1 = Label (fenetre, text = "Hello world")
+    fenetre.geometry("250x250")
+    label1 = Label (fenetre, text = "Yes Hello world")
     label1.pack()
     label1.config(fg="#FFFF00")
     fenetre.mainloop()
