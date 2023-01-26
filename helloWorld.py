86bb6608c99a04a1ba78f2429da681a093f07bf4
Raijin95120 <carlbrgs77@gmail.com>
2023-01-27 00:59:00 +0100

    color bg yellow

--- helloWorld.py
+++ helloWorld.py
@@ -4,6 +4,7 @@ def HelloWorld():
     fenetre = Tk()
     fenetre.title("Hello World")
     fenetre.geometry("250x250")
+    fenetre.config(bg = "#FFFF00")
     label1 = Label (fenetre, text = "Yes Hello world")
     label1.pack()
     label1.config(fg="#FFFF00")
