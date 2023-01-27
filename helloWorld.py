72a31e7e82aa6b37d448aae94572e992e4d71d89
Raijin95120 <carlbrgs77@gmail.com>
2023-01-27 01:26:35 +0100

    taille fenetre + alignement à gauche

--- helloWorld.py
+++ helloWorld.py
@@ -3,11 +3,12 @@ from tkinter import *
 def HelloWorld(): 
     fenetre = Tk()
     fenetre.title("Hello World")
-    fenetre.geometry("200x200")
+    fenetre.geometry("500x200")
     label1 = Label (fenetre, text = "Hello world",fg='#0000FF')
     label1.pack()
     label1.config(fg="#00FF00")
     fenetre.mainloop()
+    fenetre.geometry("-0+0")
     
     
 HelloWorld
