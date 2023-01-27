67444c4ff3fc9868f3580da4068fbf20a89e4c87
Carl Bourges <74832790+carlbrgs@users.noreply.github.com>
2023-01-27 12:01:57 +0100

    Close #3

--- helloWorld.py
+++ helloWorld.py
@@ -3,7 +3,7 @@ from tkinter import *
 def HelloWorld(): 
     fenetre = Tk()
     fenetre.title("Hello World")
-    fenetre.geometry("250x250")
+    fenetre.geometry("455x250")
     fenetre.config(bg = "#00FFFF")
     label1 = Label (fenetre, text = "Yes Hello world")
     label1.pack()
