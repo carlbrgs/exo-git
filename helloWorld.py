36b8649a44f02f979683d11d59de4c97d26a3470
carlbrgs2 <123733673+carlbrgs2@users.noreply.github.com>
2023-01-27 12:00:52 +0100

    Close #4

--- helloWorld.py
+++ helloWorld.py
@@ -3,11 +3,13 @@ from tkinter import *
 def HelloWorld(): 
     fenetre = Tk()
     fenetre.title("Hello World")
-    fenetre.geometry("250x250")
+    fenetre.geometry("300x250")
     fenetre.config(bg = "#00FFFF")
     label1 = Label (fenetre, text = "Yes Hello world")
     label1.pack()
     label1.config(fg="#00FF00")
+    labeldate = Label(fenetre, text=f"{date:%A, %B %d, %Y}", font="Calibri, 20")
+    labeldate.pack(pady=20)
     fenetre.mainloop()
     
     
